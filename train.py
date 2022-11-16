8c6837fc5cb02be9324e8a932f8fe84f7b03f542
Abdulrahman <dahoomi.ibr.mut@gmail.com>
2022-11-16 15:13:33 +0300

    knew what was the error about

--- train.py
+++ train.py
@@ -6,15 +6,15 @@ import os
 import numpy as np
 
 # Read in data
-X_train = np.genfromtxt("train_features.csv")
-y_train = np.genfromtxt("train_labels.csv")
-X_test = np.genfromtxt("test_features.csv")
-y_test = np.genfromtxt("test_labels.csv")
+X_test = np.genfromtxt("data/train_features.csv")
+y_test = np.genfromtxt("data/train_features.csv")
+X_test = np.genfromtxt("data/test_features.csv")
+y_test = np.genfromtxt("data/test_labels.csv")
 
 # Fit a model
 depth = 2
 clf = RandomForestClassifier(max_depth=depth)
-clf.fit(X_train, y_train)
+clf.fit(X_test, y_test)
 
 acc = clf.score(X_test, y_test)
 print(acc)
